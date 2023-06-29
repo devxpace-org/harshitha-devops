60697da1e12365b2200be503ba19779add7b8ab3
harshitha15devxpace <135719878+harshitha15devxpace@users.noreply.github.com>
2023-06-29 12:12:10 -0400

    Update supriya.js

--- src/supriya.js
+++ src/supriya.js
@@ -1,1 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/supriya*', (req, res) => {
+  res.send(Hi, This is Supriya!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
