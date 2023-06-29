d6ee2cbaea4878634ede776b30f9c0d659045537
harshitha15devxpace <135719878+harshitha15devxpace@users.noreply.github.com>
2023-06-29 12:12:29 -0400

    Create teju.js

---
+++ src/teju.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/teju*', (req, res) => {
+  res.send(Hi, This is teju!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
