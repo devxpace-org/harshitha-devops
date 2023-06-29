cdc74053fa3f89be9d76ef84af5f3fa7058f6bb6
harshitha15devxpace <135719878+harshitha15devxpace@users.noreply.github.com>
2023-06-29 12:13:13 -0400

    Create raja.js

---
+++ src/raja.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/raja*', (req, res) => {
+  res.send(Hi, This is Raja!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
