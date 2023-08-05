bcae9d8c84b17a6e9bd23f656b1600cd6e06d8da
harshitha15devxpace <135719878+harshitha15devxpace@users.noreply.github.com>
2023-08-05 19:24:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-  profile    = "default"
   region     = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-04505608c94c168b1"
+  ami           = "ami-0878a7bce0900eceb"
   instance_type = "t2.micro"
 }
