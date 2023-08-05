0b5542a142627803911393479529f58768cd2e05
harshitha15devxpace <135719878+harshitha15devxpace@users.noreply.github.com>
2023-08-05 19:14:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
 provider "aws" {
-    region = "us-east-2"
+  profile    = "default"
+  region     = "us-east-2"
 }
 
-resource "aws_instance" "myec2" {
-    ami = "ami-098dd3a86ea110896"
-    instance_type = "t2.micro"
-
-    tags = {
-        Name = "Terraform-ec2"
-    }
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
 }
