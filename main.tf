40207ccb880495bbc42779938ffef82886511535
harshitha15devxpace <135719878+harshitha15devxpace@users.noreply.github.com>
2023-08-05 20:15:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,4 +54,23 @@ tags = {
     }
 }
 
+resource "aws_route_table" "public-subnet1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 
